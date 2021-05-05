8175a1df29a8f55bd1bd52d600cf8912a05af47d
rob-derosa <rob-derosa@github.com>
2021-05-05 10:58:56 -0600

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,81 @@
+import * as core from "@actions/core";
+import * as github from '@actions/github'
+
+type UnassignedIssue = {
+  issue: any;
+  priority: string;
+  priorityRank: number;
+};
+
+async function run(): Promise<void> {
+  try {
+    const prioritiesString = "blocker,critical,important"
+    const gitHubToken = core.getInput("github-token", { required: true })
+    const context = github.context;
+
+    console.log(context.payload);
+
+    const client = github.getOctokit(gitHubToken);
+    const priorities = prioritiesString.split(',');
+    const issues = await client.paginate(client.rest.issues.listForRepo, {
+      owner: "githubcustomers",
+      repo: "linkedin",
+      per_page: 200,
+    });
+
+    const unassignedIssues = new Array<any>();
+    for (const issue of issues) {
+      if (issue.state == "closed")
+        continue;
+
+      for (const label of issue.labels) {
+        if (priorities.indexOf(label.name) > -1) {
+          if (issue.assignees.length == 0) {
+            const unassignedIssue: UnassignedIssue = {
+              issue: issue,
+              priority: label.name,
+              priorityRank: priorities.indexOf(label.name),
+            };
+            unassignedIssues.push(unassignedIssue);
+          }
+        }
+      }
+    }
+
+    var sortedIssues: UnassignedIssue[] = unassignedIssues.sort((n1, n2) => {
+      if (n1.priorityRank > n2.priorityRank) {
+        return 1;
+      }
+
+      if (n1.priorityRank < n2.priorityRank) {
+        return -1;
+      }
+
+      return 0;
+    });
+
+    let output = "";
+    output += `## Issues Needing an Owner\n`;
+    let lastPriority;
+    for (const unassignedIssue of sortedIssues) {
+
+      if(lastPriority != unassignedIssue.priority){
+        lastPriority = unassignedIssue.priority;
+        output += `### ${lastPriority}\n`;
+      }
+
+      //console.log(unassignedIssue.issue);
+      output += `* [Issue #${unassignedIssue.issue.number}](${unassignedIssue.issue.html_url}): ${unassignedIssue.issue.title}\n`;
+    }
+
+    let gist = await client.gists.create({ description: "Prioritized + Unassigned Issues", files: { [ "report.md"] : {content: output.toString()}}});
+    console.log(gist.data.html_url);
+    //console.log(output.toString());
+
+  } catch (error) {
+    console.log(error);
+    core.setFailed(error.message)
+  }
+}
+
+run()
